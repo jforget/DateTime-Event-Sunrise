8b0100dbcede1c9be1594a82b3c9ecb7d7ebf769
Jean <J2N-FORGET@orange.fr>
2013-08-06 22:20:14 +0200

    Clean up the license issues, change the version number in prevision of the next release.

--- lib/DateTime/Event/Sunrise.pm
+++ lib/DateTime/Event/Sunrise.pm
@@ -1,20 +1,8 @@
 #
 #     Perl DateTime extension for computing the sunrise/sunset on a given day
-#     Copyright (C) 1999-2013 Ron Hill and Jean Forget
-# 
-#     This program is free software; you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation; either version 1, or (at your option)
-#     any later version.
-# 
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-# 
-#     You should have received a copy of the GNU General Public License
-#     along with this program; if not, write to the Free Software Foundation,
-#     Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#     Copyright (C) 1999-2004, 2013 Ron Hill and Jean Forget
+#
+#     See the license in the embedded documentation below.
 #
 package DateTime::Event::Sunrise;
 
@@ -29,7 +17,7 @@ use Params::Validate qw(:all);
 use Set::Infinite qw(inf $inf);
 use vars qw( $VERSION $RADEG $DEGRAD @ISA );
 @ISA     = qw( Exporter );
-$VERSION = '0.0502';
+$VERSION = '0.0503';
 $RADEG   = ( 180 / pi );
 $DEGRAD  = ( pi / 180 );
 my $INV360 = ( 1.0 / 360.0 );
@@ -1092,7 +1080,35 @@ for suggestions.
 
 =head1 COPYRIGHT and LICENSE
 
-Here is the copyright information provided by Paul Schlyter:
+=head2 Perl Module
+
+This program is distributed under the same terms as Perl:
+GNU Public License version 1 or later and Perl Artistic License
+
+You can find the text of the licenses in the F<LICENSE> file or at
+L<http://www.perlfoundation.org/artistic_license_1_0>
+and L<http://www.gnu.org/licenses/gpl-1.0.html>.
+
+Here is the summary of GPL:
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 1, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+=head2 Original C program
+
+Here is the copyright information provided by Paul Schlyter
+for the original C program:
 
 Written as DAYLEN.C, 1989-08-16
 
